4e713f3edb60bb1fb24216dfd05625171d61a318
Ianto Lin Xi <ianto@kite.com>
2018-11-21 12:51:02 -0500

    switch back wrong repo

--- evaluate.py
+++ evaluate.py
@@ -218,7 +218,7 @@ def plot_confusion_matrix(data, results):
         ax=ax,
     )
     plt.xlabel('prediction', axes=ax)
-    plt.ylabel('label', axes=ax)
+    plt.ylabel('diagnosis by MRI or histopathology', axes=ax)
     return fig
 
 
